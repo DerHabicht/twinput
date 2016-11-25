146b27235089a9186ef90f41b57c792364e65b3e
Robert Herschel Hawk <robert@the-hawk.us>
2016-11-25 09:57:05 -0700

    Added TODO parsing from a Git repo
    When passed the -g flag, twinput will now read comments in a Git repo
    using the git grep command. The comments will then be formatted
    according to a template file and added to the Taskwarrior database.
    
    The syntax for adding a task has also been modified to be more
    compliant with the Todo.txt standard. Quotation marks around the task
    description are no longer required.
    
    twinput will also generate an MD5 hash of the task description when
    adding a task. The hash is stored in the Taskwarrior database as a UDA.
    If twinput can retrieve a task with a matching hash, then twinput will
    just update the task rather than creating a new one. This is primarily
    intended to keep the gitgrep mode from parsing the same comment into
    the database several times.
    
    When in interactive mode, twinput will collect any errors generated
    while parsing and re-open the input buffer presenting the errors and
    their error messages to the user for remodification. As a part of this
    more elegant error handling process, errored tasks are no longer
    deleted when parsing fails, but rather the hashing functionality above
    allows the user to update or overwrite.

--- twinput.py
+++ twinput.py
@@ -1,12 +1,33 @@
-#!/bin/python3
+#!/usr/bin/python3
 
-import tempfile, os, re
+# Copyright (C) 2016 Robert Herschel Hawk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from hashlib import md5
+from re import finditer, search, sub
+from os import environ
+from tempfile import NamedTemporaryFile
 from taskw import TaskWarrior
 from taskw.exceptions import TaskwarriorError
-from subprocess import call
+from subprocess import call, run, PIPE
+from sys import argv, exit
 
 # Get the system editor, defaulting to Vim
-EDITOR = os.environ.get("EDITOR", "vim")
+EDITOR = environ.get("EDITOR", "vim")
+VERSION = "TaskWarrior Input 0.2.0a"
 
 # Load TaskWarrior
 taskw = TaskWarrior()
@@ -14,7 +35,9 @@ taskw = TaskWarrior()
 
 def parse_todos(text):
     lines = text.split("\n")
+    failed_lines = []
 
+    # TODO: Handle a line of spaces
     for line in lines:
         try:
             if line[0] == "#":
@@ -22,48 +45,73 @@ def parse_todos(text):
         except IndexError:
             continue
 
-        # Get task description
-        try:
-            description = re.search(r"\"(.+)\"", line).group(1)
-        except AttributeError:
-            print("Couldn't parse description for " + line)
-            continue
 
-        # Get the Taskwarrior task
-        try:
-            task = taskw.task_add(description)
-            task_id = task["id"]
-        except TaskwarriorError as err:
-            msg = err.stderr.decode("utf-8").split("\n")[-1]
-            print("Adding task " + description + " failed:\n\t" + msg)
-            continue
+        # Metadata will be trimmed from the line.
+        # The leftovers will be interpreted as the task description.
+        description = line
+
+        # Build a dictionary that will become the task
+        task_data = {}
 
         # Get the task priority
         try:
-            task["priority"] = re.search(r"\(([UHMNLT])\)", line).group(1)
+            task_data["priority"] = search(r"^\((\w+)\)", line).group(1)
+            description = sub(r"^\(\w+\)", "", description)
         except AttributeError:
             pass
 
         # Get the task project
         try:
-            task["project"] = re.search(r"\+(\S+)", line).group(1)
+            task_data["project"] = search(r"\+(\S+)", line).group(1)
+            description = sub(r"\+\S+", "", description)
         except AttributeError:
             pass
 
         # Get the task context(s)
-        task["tags"] = []
-        for match in re.finditer(r"@(\S+)", line):
+        task_data["tags"] = []
+        for match in finditer(r"@(\S+)", line):
             try:
-                task["tags"].append(match.group(1))
+                task_data["tags"].append(match.group(1))
             except AttributeError:
                 pass
 
+        description = sub(r"@\S+", "", description)
+
         # Get any other tags
-        for match in re.finditer(r"(\w+):(\S+)", line):
+        for match in finditer(r"(\w+):(\S+)", line):
             try:
-                task[match.group(1)] = match.group(2)
+                task_data[match.group(1)] = match.group(2)
             except (AttributeError, IndexError):
                 pass
+
+        description = sub(r"\w+:\S+", "", description)
+        description = description.strip()
+
+        if description == "":
+            msg = "No description found."
+            failed_lines.append((line, msg))
+            continue
+
+        # Hash the input description case insensitively.
+        twi_hash = md5(description.upper().encode("utf-8")).hexdigest()
+
+        # Try to retrieve the task by its hash. If we find it, we'll update
+        # instead of creating a new task.
+        (task_id, task) = taskw.get_task(twi_hash=twi_hash)
+
+        if not task_id:
+            # Task not found, so we attempt to create the task
+            try:
+                task = taskw.task_add(description, twi_hash=twi_hash)
+                task_id = task["id"]
+
+                for key, value in task_data.items():
+                    task[key] = value
+
+            except TaskwarriorError as err:
+                msg = err.stderr.decode("utf-8").split("\n")[-1]
+                failed_lines.append((line, msg))
+                continue
 
         # Attempt to update tasks
         try:
@@ -76,20 +124,40 @@ def parse_todos(text):
                 taskw.task_update(task)
         except TaskwarriorError as err:
             msg = err.stderr.decode("utf-8").split("\n")[-1]
-            print("Adding task " + description + " failed:\n\t" + msg)
-            taskw.task_delete(id=task_id)
+            failed_lines.append((line, msg))
+            continue
 
 
-def main():
+    return failed_lines
+
+
+def open_file_buffer(lines=None):
+    # TODO: Build buffer header as a regular string and then encode
     # Initialize the file buffer
-    initial_message = b"# TaskWarrior Input 0.1.3\n"
-    initial_message += b"# www.the-hawk.us\n"
-    initial_message += b"#\n# Input tasks in the form:\n"
-    initial_message += b"#    (N) \"Task\" @CONTEXT +PROJECT due: scheduled:"
-    initial_message += b"\n\n"
+    initial_message = (b"# " + VERSION.encode("utf-8") + b"\n"
+                       + b"# www.the-hawk.us\n"
+                       + b"#\n# Input tasks in the form:\n"
+                       + b"#    (N) Task @CONTEXT +PROJECT due: scheduled:"
+                       + b"\n\n")
+
+    if lines:
+        initial_message += (b"# The following lines were not successfully "
+                            b"imported into Taskwarrior.\n"
+                            b"# Uncomment to re-attempt parsing or leave "
+                            b"commented to abort.\n"
+                            b"# In some cases, the task might have been added,"
+                            b"but some parts didn't parse.\n"
+                            b"# As long as you don't change the description "
+                            b"(case-insensitive) the task will be updated, "
+                            b"not duped.")
+
+        for line in lines:
+            initial_message += (b"# " + line[0].encode("utf-8")
+                                + b"\n#    ERROR: "
+                                + line[1].encode("utf-8") + b"\n\n")
 
     # Open editor for input
-    with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
+    with NamedTemporaryFile(suffix=".tmp") as tf:
         # Create a temporary file
         tf.write(initial_message)
         tf.flush()
@@ -98,10 +166,74 @@ def main():
         call([EDITOR, tf.name])
 
         # Retrieve the edited file
-        tf.seek(0)
-        edited_message = tf.read().decode("utf-8")
+        # NOTE: This almost works for making the script wait on a gui editor
+        #       HOWEVER, this will make the script hang if the editor is closed
+        #       without changing the file. So,
+        # FIXME: Make this not hang if the file is not changed
 
-    parse_todos(edited_message)
+        #while True:
+        #    tf.seek(0)
+        #    if tf.read() != initial_message:
+        #        break
+
+        tf.seek(0)
+        return tf.read().decode("utf-8")
+
+
+def print_usage():
+    print("\n    " + VERSION)
+    print("    www.the-hawk.us")
+    print("\n    Usage: " + argv[0] + " [-e|--editor {EDITOR}]\n")
+
+    exit()
+
+
+def run_git_grep():
+    with open(".twparse", "r") as template_file:
+        template = template_file.readline()
+
+    print(template)
+
+    result = run(["git", "grep", "TODO:"], stdout=PIPE).stdout.decode()
+    lines = result.split("\n")
+
+    text = ""
+    for line in lines:
+        try:
+            task = search(r"\s+TODO:\s(.+)", line).group(1)
+            task = sub(r"\$TODO", task, template)
+            text += task
+        except AttributeError:
+            pass
+
+    parse_todos(text)
+
+
+def get_direct_input():
+    failed = None
+    while True:
+        to_parse = open_file_buffer(lines=failed)
+        failed = parse_todos(to_parse)
+        if not failed:
+            break
+
 
 if __name__ == "__main__":
-    main()
+    if len(argv) > 1:
+        skip = [0]
+        for index, arg in enumerate(argv):
+            # Ignore the script name
+            if index in skip:
+                continue
+
+            if arg == "-e" or arg == "--editor":
+                try:
+                    EDITOR = argv[index + 1]
+                    skip.append(index + 1)
+                except IndexError:
+                    print_usage()
+            elif arg == "-g" or arg == "--gitgrep":
+                run_git_grep()
+            else:
+                get_direct_input()
+
